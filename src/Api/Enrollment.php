6f5c6edd00c79e9c346db8a1edc94acc5c824080
Artis Bajārs <artis@Artiss-MacBook-Pro.local>
2017-06-08 13:04:02 +0300

    Formatting

--- src/Api/Enrollment.php
+++ src/Api/Enrollment.php
@@ -4,32 +4,29 @@ namespace Suru\Docebo\DoceboApiWrapper\Api;
 
 class Enrollment extends BaseEndpoint {
 
-    /**
-     * Enrollment endpoints.
-     *
-     * @var array
-     */
-    protected $endpoints = [
-        'list'  => '/api/enroll/enrollments',
+  /**
+   * Enrollment endpoints.
+   *
+   * @var array
+   */
+  protected $endpoints = [
+    'list'  => '/api/enroll/enrollments',
+  ];
+
+  /**
+   * Get enrollments by course.
+   *
+   * @param int $course_id
+   * @return array
+   */
+  public function getEnrollments($course_id = NULL) {
+    $headers = $this->master->getAccessTokenHeader();
+    $parameters = [
+      'id_course' => $course_id,
     ];
-
-    /**
-     * Get enrollments by course.
-     *
-     * @param int $course_id
-     * @return array
-     */
-    public function getEnrollments($course_id = NULL)
-    {
-		$headers = $this->master->getAccessTokenHeader();
-
-		$parameters = [
-		  'id_course' => $course_id,
-        ];
-
-		$response = $this->master->call($this->endpoints['list'], $headers, $parameters);
-
-		return $response->body;
-    }
+    $response = $this->master->call($this->endpoints['list'], $headers, $parameters);
+    
+    return $response->body;
+  }
 
 }
