61d51481de8ad5f96a303061d4f119d515c0a4d0
Artis Bajārs <artis@Artiss-MacBook-Pro.local>
2017-06-12 14:00:47 +0300

    Use the full array of filter parametrs available for Enrollments

--- src/Api/Enrollment.php
+++ src/Api/Enrollment.php
@@ -15,16 +15,28 @@ class Enrollment extends BaseEndpoint {
   ];
 
   /**
-   * Get enrollments by course.
+   * List enrollments, filered by user-defined criteria.
    *
-   * @param int $course_id
+   * Possible parameters:
+   *
+   * id_user (integer, optional): numeric ID of an user
+   * id_course (string, optional): nuemric ID of a course
+   * updated_from (string, optional): additional filter on enrollments, date filter on "last_access" property
+   * user_ext_type (string, optional): external user type
+   * user_ext (string, optional): external user ID
+   * from (integer, optional): limit the resultset by record offset
+   * count (integer, optional): limit the resultset by number of records
+   * start_date (string, optional): limit the resultset by date (yyyy-MM-dd HH:mm:ss format, UTC timezone)
+   * end_date (string, optional): limit the resultset by date (yyyy-MM-dd HH:mm:ss format, UTC timezone)
+   * username (string, optional): limit the resultset by username
+   * completed_from (string, optional): the start date & time used to filter enrollments based on the completion date (in yyyy-MM-dd HH:mm:ss format, UTC timezone)
+   * completed_to (string, optional): the end date & time used to filter enrol
+   *
+   * @param array $parameters
    * @return array
    */
-  public function getEnrollments($course_id = NULL) {
+  public function getEnrollments($parameters = []) {
     $headers = $this->master->getAccessTokenHeader();
-    $parameters = [
-      'id_course' => $course_id,
-    ];
     $enrollments = [];
     $response = $this->master->call($this->endpoints['list'], $headers, $parameters);
     foreach ($response->body->enrollments as $enrollment) {
